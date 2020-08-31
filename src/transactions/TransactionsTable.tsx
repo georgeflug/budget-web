55d3866e16b3cdd7a749a5e92fa9689c33ab1e3a
Richard White <georgeflug@gmail.com>
2020-08-30 22:57:01 -0500

    [Show Transactions] Extract transactions table to own component.

---
+++ src/transactions/TransactionsTable.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {TableCell} from "@material-ui/core";
+import TableRow from "@material-ui/core/TableRow";
+import TableBody from "@material-ui/core/TableBody";
+import TableHead from "@material-ui/core/TableHead";
+import Table from "@material-ui/core/Table";
+import Paper from "@material-ui/core/Paper";
+import TableContainer from "@material-ui/core/TableContainer";
+import {format} from 'date-fns';
+import {Transaction} from "./Transaction";
+
+export function TransactionsTable(props: { transactions: Transaction[] }) {
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell align="right">Amount</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.transactions.map((row: any) => (
+              <TableRow key={row.recordId}>
+                <TableCell>{format(row.postedDate, 'MMM dd')}</TableCell>
+                <TableCell>{row.postedDescription}</TableCell>
+                <TableCell align="right">{row.totalAmount}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
