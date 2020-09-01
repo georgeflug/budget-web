f8141ba33e33149936ef9f3f38067217b0503914
Richard White <georgeflug@gmail.com>
2020-08-31 21:03:36 -0500

    [Edit] Improve display of filters

--- src/transactions/TransactionFilters.tsx
+++ src/transactions/TransactionFilters.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import {CategoryFilter} from "./filters/categoryFilter/CategoryFilter";
 import {DateFilter} from "./filters/dateFilter/DateFilter";
+import {Grid} from "@material-ui/core";
 
 export function TransactionFilters() {
   return (
-    <div>
-      <CategoryFilter/>
-      <DateFilter/>
-    </div>
+    <Grid container>
+      <Grid item xs={4} md={2} lg={1}>
+        <CategoryFilter/>
+      </Grid>
+      <Grid item xs={4} md={2} lg={1}>
+        <DateFilter/>
+      </Grid>
+    </Grid>
   );
 }
