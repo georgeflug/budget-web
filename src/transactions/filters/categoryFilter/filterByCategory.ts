1cfe7c5fc160fcea1f007d865e3f2fb02b84dbb6
Richard White <georgeflug@gmail.com>
2020-08-31 20:21:13 -0500

    [Edit] Fix 'To be determined' should show stuff with no category

--- src/transactions/filters/categoryFilter/filterByCategory.ts
+++ src/transactions/filters/categoryFilter/filterByCategory.ts
@@ -4,7 +4,7 @@ export const filterByCategory = (transaction: Transaction, category: string) => 
   if (category === 'All Categories') {
     return true;
   }
-  if (category === 'To be determined' && transaction.splits.some(split => !!split.budget)) {
+  if (category === 'To be determined' && transaction.splits.some(split => !split.budget)) {
     return true;
   }
   return transaction.splits.some(split => split.budget === category);
