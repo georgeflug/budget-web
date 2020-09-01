458ac731438f0415bb5ce81a0e1e25d0f31fb666
Richard White <georgeflug@gmail.com>
2020-08-31 20:59:38 -0500

    [Performance] Greatly increase speed by paginating transaction table

--- src/transactions/TransactionsTable.tsx
+++ src/transactions/TransactionsTable.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {TableCell} from "@material-ui/core";
+import {TableCell, TablePagination} from "@material-ui/core";
 import TableRow from "@material-ui/core/TableRow";
 import TableBody from "@material-ui/core/TableBody";
 import TableHead from "@material-ui/core/TableHead";
@@ -10,6 +10,21 @@ import {Transaction} from "./transactionModel";
 import {TransactionRow} from "./TransactionRow";
 
 export function TransactionsTable(props: { transactions: Transaction[] }) {
+  const transactions = props.transactions;
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const pagedTransactions = transactions.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+
   return (
     <div>
       <TableContainer component={Paper}>
@@ -23,10 +38,19 @@ export function TransactionsTable(props: { transactions: Transaction[] }) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {props.transactions.map((row: Transaction) => (<TransactionRow row={row}/>))}
+            {pagedTransactions.map((row: Transaction) => (<TransactionRow row={row}/>))}
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 100, 1000, 10000]}
+        component="div"
+        count={transactions.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
     </div>
   );
 }
