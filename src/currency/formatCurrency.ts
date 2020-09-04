c3c9bf1f5721f64dc3eef595ff910185057eb252
Richard White <georgeflug@gmail.com>
2020-09-04 01:26:49 -0500

    [Cleanup] Show income as positive green numbers

--- src/currency/formatCurrency.ts
+++ src/currency/formatCurrency.ts
@@ -1,10 +1,20 @@
+import React from "react";
+
 const numberFormatExact = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
 const numberFormatRounded = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0});
 
 export function formatCurrencyExact(amount: number) {
-  return numberFormatExact.format(amount);
+  return goGreen(amount, numberFormatExact);
 }
 
 export function formatCurrencyRounded(amount: number) {
-  return numberFormatRounded.format(amount);
+  return goGreen(amount, numberFormatRounded);
+}
+
+function goGreen(amount: number, formatter: Intl.NumberFormat) {
+  if (amount < 0) {
+    const text = formatter.format(-amount);
+    return React.createElement('div', { style: { color: 'green' } }, `+${text}`);
+  }
+  return formatter.format(amount);
 }
