c9133fef11679df426f90eccedbba2f269c336e4
Richard White <georgeflug@gmail.com>
2020-09-10 17:21:28 -0500

    [Cleanup] Add ability to search with quotes

--- src/transactions/filters/searchFilter/filterBySearchText.ts
+++ src/transactions/filters/searchFilter/filterBySearchText.ts
@@ -1,11 +1,23 @@
 import {Transaction} from "../../transactionModel";
 
+let rememberedTokens: string[] = [];
+let lastSearchText = '';
+
 export const filterBySearchText = (transaction: Transaction, searchText: string) => {
   if (!searchText) {
     return true;
   }
-  return searchText.toLowerCase().split(' ')
+  if (searchText !== lastSearchText) {
+    lastSearchText = searchText;
+    rememberedTokens = splitByWordOrQuotes(lastSearchText.toLowerCase());
+  }
+  return rememberedTokens
     .filter(token => !!token)
     .some(token => (transaction.postedDescription || '').toLowerCase().includes(token)
       || (transaction.notes || '').toLowerCase().includes(token));
 };
+
+function splitByWordOrQuotes(text: string): string[] {
+  return (text.match(/(\w+)|"([^"]+)"/g) || [])
+    .map(token => token.replace(/^"(.*)"$/, '$1'))
+}
