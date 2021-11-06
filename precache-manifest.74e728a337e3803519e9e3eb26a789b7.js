af6b392f8b1a5a8620c524c1c2c3f58cdc1acd10
georgeflug <georgeflug@gmail.com>
2021-11-06 02:32:34 +0000

    Deploying to gh-pages from @ georgeflug/budget-web@39436bb51a18f107742a22ce5073a4ec3ed9f5b1 🚀

---
+++ precache-manifest.74e728a337e3803519e9e3eb26a789b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e4d264737425917ee3660c0cd4c2b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4725a0737109afa9897c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "408581fb68adef285603",
+    "url": "/static/js/2.35f814d4.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.35f814d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4725a0737109afa9897c",
+    "url": "/static/js/main.fd7ca5d4.chunk.js"
+  },
+  {
+    "revision": "695c97cebe2d75f0f165",
+    "url": "/static/js/runtime-main.a99a96f1.js"
+  }
+]);
